3f96c2e691c0e5aadf32f90b987777258cdf746d
JheysonGalvis <tecnojheyson@gmail.com>
2024-09-07 11:10:14 -0500

    abordamos tipos de datos

---
+++ tipos-de-datos.py
@@ -0,0 +1,17 @@
+"string" #string se utiliza para cadenas de texto
+'string' #así también podemos generar un string
+"Hola mundo"
+'Hola mundo'
+
+"""Tus datos son:
+   nombre: Jheyson
+   apellido: Galvis"""
+'''Tus datos son:
+   nombre: Jheyson
+   apellido: Galvis'''
+#En Python tenemos diferentes tipos de números
+4 #número entero
+40.2#número tipo float
+#También tenemos el tipo de datos boolean
+True #verdadero
+False #falso
